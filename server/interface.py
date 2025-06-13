0243a79e1fd252cee96a9115473d549fb25ee0a3
Kaitlin Martin <kxm1944@mavs.uta.edu>
2025-06-12 20:29:07 -0500

    comment interface.py

--- server/interface.py
+++ server/interface.py
@@ -4,13 +4,14 @@ import time
 arduino_ip = "192.168.50.169"
 
 def pump():
-    # Turn pump ON
+    # Turns pump on for specified amount of time, then turns off
     pump_on()
     time.sleep(5)
     pump_off()
     return
 
 def pump_on():
+    # Turn pump ON
     response = requests.get(f"http://{arduino_ip}/pump/on")
     print("Pump ON:", response.status_code)
     return
@@ -22,9 +23,13 @@ def pump_off():
     return
 
 def get_json():
+    # retrieves json in the form of a list of dictionaries (for each time stamp)
     response = requests.get(f"http://{arduino_ip}/data.json")
+    print(type(response))
     data = response.json()
     return data
 
-# pump()
-print(get_json())
+# uncomment to test the pump_on() and pump_off() functions
+# pump()            
+json = get_json()
+print(json)
