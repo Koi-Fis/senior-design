629ab9e95f74caf0e737d4a00b8fc58b1ec3441c
chanuthi <vidara54@gmail.com>
2025-06-24 18:56:25 -0500

    added the correct ip and the pump can be truned on from the elecron app

--- egg/src/care-schedule/care-schedule.tsx
+++ egg/src/care-schedule/care-schedule.tsx
@@ -3,7 +3,7 @@ import './care-schedule.css';
 function CareSchedule() {
   const turnOnPump = async() => {
     try{
-      const response = await fetch("http://192.168.x.x/pump");
+      const response = await fetch("http://192.168.50.169/fan2/off");
       if (!response.ok) throw new Error('HTTP error! status: Failed to turn on pump');
       console.log("Pump turned on successfully");
       } catch (error) {
@@ -13,9 +13,11 @@ function CareSchedule() {
 
   return(
     <div className ="landing-page">
+      <div className = "schedule-container">
+        
+        <button onClick={turnOnPump} className="watering-btn">Watering</button>
 
-
-      <button onClick={turnOnPump} className="watering-btn">Watering</button>
+      </div>
       
     </div>
   )
