53eeee049eec798202631ac40f73c2ccf573f850
chanuthi <vidara54@gmail.com>
2025-07-01 17:06:09 -0500

    added bootstrap

--- egg/src/care-schedule/care-schedule.tsx
+++ egg/src/care-schedule/care-schedule.tsx
@@ -1,9 +1,13 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Dropdown from 'react-bootstrap/Dropdown';
+import DropdownButton from 'react-bootstrap/DropdownButton';
 import './care-schedule.css';
+
 
 function CareSchedule() {
   const turnOnPump = async() => {
     try{
-      const response = await fetch("http://192.168.50.169/fan2/off");
+      const response = await fetch("http://192.168.50.169/pump/off");
       if (!response.ok) throw new Error('HTTP error! status: Failed to turn on pump');
       console.log("Pump turned on successfully");
       } catch (error) {
@@ -14,9 +18,19 @@ function CareSchedule() {
   return(
     <div className ="landing-page">
       <div className = "schedule-container">
-        
-        <button onClick={turnOnPump} className="watering-btn">Watering</button>
 
+      <DropdownButton className = "schedule-drop" id="dropdown-basic-button" title="Select a Scheduler">
+      <Dropdown.Item href="#/action-1">schedule water</Dropdown.Item>
+      <Dropdown.Item href="#/action-2">schedule fan</Dropdown.Item>
+      <Dropdown.Item href="#/action-3">schedule grow light</Dropdown.Item>
+      </DropdownButton>
+      </div>
+
+      <div className = "manual-ctrl">
+
+        <button onClick={turnOnPump} className="watering-btn manual-btns">Watering</button> 
+        <button className="Fan-btn manual-btns">Fan</button> 
+        <button className="Light-btn manual-btns">Grow Light</button> 
       </div>
       
     </div>
