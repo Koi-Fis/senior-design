39de4fe9c6cc7d0920d145c28b134caed1179c37
Sam <112108749+samh318@users.noreply.github.com>
2025-07-03 15:39:52 -0500

    cleaning up sqlite_test.py, adding print_database.py

--- egg/backend/sqlite_test.py
+++ egg/backend/sqlite_test.py
@@ -1,8 +1,13 @@
+
+# Make sure to run from egg/backend, with "python sqlite_test.py"
+# To run with the daily average code, run with any additional cmd line parameter, ex. "python sqlite_test a"
+
 import sqlite3
 import urllib.request
 import json
 import time
 import interface
+import sys
 
 refresh_rate = 300 # 5 mins
 refresh_limit = 86400 # 1 day
@@ -12,7 +17,7 @@ refresh = 1
 while(True):
 
     print()
-    print("refresh", refresh, '\n')
+    print("Refresh:", refresh, '\n')
     with sqlite3.connect('egg_test.db') as conn:    
         cursor = conn.cursor()    
         cursor.execute('''CREATE TABLE IF NOT EXISTS eggbase (
@@ -27,13 +32,8 @@ while(True):
 	    "potassium"	REAL,
 	    PRIMARY KEY("timestamp"))''')
 
-        #with urllib.request.urlopen("http://192.168.50.169/data.json") as url: 
         data = interface.get_json()
         #print(json.dumps(data, indent = 1), '\n')
-
-        #with open('data.json', 'r') as file:
-            #data = json.load(file)
-            #print(json.dumps(data, indent = 1), '\n')
 
         for item in data:
             val = (item['timestamp'], item['temperature_celcius'], item['humidity'], item['heat_index_celcius'], item['moisture_one'],
@@ -42,54 +42,34 @@ while(True):
                   "moisture_two, nitrogen, phosphorus, potassium) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)"
             cursor.execute(sql, val)
 
-        '''
-        cursor.execute("SELECT * FROM eggbase")
-        entries = cursor.fetchall()
-        #print(entries, '\n')
-        #print(len(entries), "entries", '\n')
-
-        count = 1
-        for entry in entries:
-            timestamp, temp, humidity, heat, m1, m2, n, p, k = entry
-            print("ENTRY", count)
-            print("timestamp:    ", timestamp)
-            print("temperature:  ", temp)
-            print("humidity:     ", humidity)
-            print("heat index:   ", heat)
-            print("moisture one: ", m1)
-            print("moisture two: ", m2)
-            print("nitrogen:     ", n)
-            print("phosphorus:   ", p)
-            print("potassium:    ", k, '\n')
-            count += 1
-        '''
-
+        # Daily averaging code
+        if(len(sys.argv) > 1):
         #if(refresh * refresh_rate > refresh_limit):
-        val = (daily_entries,)
-        sql = "SELECT * FROM(SELECT * FROM eggbase ORDER BY timestamp DESC) AS sub ORDER BY timestamp DESC LIMIT ?;"
-        cursor.execute(sql, val)
+            val = (daily_entries,)
+            sql = "SELECT * FROM(SELECT * FROM eggbase ORDER BY timestamp DESC) AS sub ORDER BY timestamp DESC LIMIT ?;"
+            cursor.execute(sql, val)
 
-        entries = cursor.fetchall()
-        avg_temp, avg_humidity, avg_heat, avg_m1, avg_m2, avg_n, avg_p, avg_k = 0.0, 0.0, 0.0, 0, 0, 0.0, 0.0, 0.0
+            entries = cursor.fetchall()
+            avg_temp, avg_humidity, avg_heat, avg_m1, avg_m2, avg_n, avg_p, avg_k = 0.0, 0.0, 0.0, 0, 0, 0.0, 0.0, 0.0
 
-        for entry in entries:
-            timestamp, temp, humidity, heat, m1, m2, n, p, k = entry
-            avg_temp += float(temp)
-            avg_humidity += float(humidity)
-            avg_heat += float(heat)
-            avg_m1 += int(m1)
-            avg_m2 += int(m2)
-            avg_n += float(n)
-            avg_p += float(p)
-            avg_k += float(k)
-        avg_temp /= len(entries)
-        avg_humidity /= len(entries)
-        avg_heat /= len(entries)
-        avg_m1 /= len(entries)
-        avg_m2 /= len(entries)
-        avg_n /= len(entries)
-        avg_p /= len(entries)
-        avg_k /= len(entries)
+            for entry in entries:
+                timestamp, temp, humidity, heat, m1, m2, n, p, k = entry
+                avg_temp += float(temp)
+                avg_humidity += float(humidity)
+                avg_heat += float(heat)
+                avg_m1 += int(m1)
+                avg_m2 += int(m2)
+                avg_n += float(n)
+                avg_p += float(p)
+                avg_k += float(k)
+            avg_temp /= len(entries)
+            avg_humidity /= len(entries)
+            avg_heat /= len(entries)
+            avg_m1 /= len(entries)
+            avg_m2 /= len(entries)
+            avg_n /= len(entries)
+            avg_p /= len(entries)
+            avg_k /= len(entries)
 
     time.sleep(60)
     refresh += 1
