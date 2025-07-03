040e69282ff1dc5715fc1108651d3576a817a5fc
Sam <112108749+samh318@users.noreply.github.com>
2025-07-03 18:15:48 -0500

    fans in tasks.py

--- egg/backend/tasks.py
+++ egg/backend/tasks.py
@@ -17,19 +17,41 @@ def check_sensors():
 
     return
 
+def run_fan1(period):
+    period = int(period)*60 # input recast as an int and converted from minutes to seconds
+
+    interface.fan1_on()
+    time.sleep(period)
+    interface.fan1_off()
+
+    return
+
+def run_fan2(period):
+    period = int(period)*60 # input recast as an int and converted from minutes to seconds
+
+    interface.fan2_on()
+    time.sleep(period)
+    interface.fan2_off()
+
+    return
+
 def main():
-    match(sys.argv[0].lower()):
+    match(sys.argv[1].lower()):
         case "water_plants":
             # python water_plants.py minutes
-            water_plants(sys.argv[1])
+            water_plants(sys.argv[2])
         case "check_sensors":
             # python check_sensors.py
             check_sensors()
+        case "run_fan1":
+            run_fan1(sys.argv[2])
+        case "run_fan2":
+            run_fan2(sys.argv[2])
         case default:
             print("function not found")
             exit()
             
     return
     
-if __name__ == "__name__":
+if __name__ == "__main__":
     main()
