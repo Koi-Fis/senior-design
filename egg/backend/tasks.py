0e11925a7e8eb377411a93d801082af93a626976
David Denny <djd5603@mavs.uta.edu>
2025-07-08 17:01:43 -0500

    combined the fan tasks in task.py

--- egg/backend/tasks.py
+++ egg/backend/tasks.py
@@ -17,40 +17,33 @@ def check_sensors():
 
     return
 
-def run_fan1(period):
+def run_fans(period):
     period = int(period)*60 # input recast as an int and converted from minutes to seconds
 
     interface.fan1_on()
-    time.sleep(period)
-    interface.fan1_off()
-
-    return
-
-def run_fan2(period):
-    period = int(period)*60 # input recast as an int and converted from minutes to seconds
-
     interface.fan2_on()
+    
     time.sleep(period)
+
+    interface.fan1_off()
     interface.fan2_off()
 
     return
 
 def main():
-    match(sys.argv[1].lower()):
-        case "water_plants":
-            # python water_plants.py minutes
+    run = sys.argv[1].lower()
+    if run=="water_plants":
+        # python water_plants.py minutes
             water_plants(sys.argv[2])
-        case "check_sensors":
-            # python check_sensors.py
+    elif run=="check_sensors":
+        # python check_sensors.py
             check_sensors()
-        case "run_fan1":
-            run_fan1(sys.argv[2])
-        case "run_fan2":
-            run_fan2(sys.argv[2])
-        case default:
-            print("function not found")
-            exit()
-            
+    elif run=="run_fans":
+        run_fans(sys.argv[2])
+    else:
+        print("function not found")
+        exit()
+                     
     return
     
 if __name__ == "__main__":
