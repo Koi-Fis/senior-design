dfa2aba45e6855dd02330f67d2ccb96b6d8eb0e7
chanuthi <chanuthi.subasinghage@mavs.uta.edu>
2025-07-08 07:04:08 -0500

    changes to care page

---
+++ egg/src/care-schedule/water-scheduler.tsx
@@ -0,0 +1,12 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './care-schedule.css';
+
+function App()
+{
+    return (
+        <div className="app-shell">
+        </div>
+    );
+}
+
+export default App;
