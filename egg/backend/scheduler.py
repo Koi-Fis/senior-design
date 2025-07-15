3d4e377e5190d1baafa5a63d249c77333c63059b
Koi-Fis <khoifish23@gmail.com>
2025-07-15 18:37:18 -0500

    scheduler.py is able to update the database regularly, jobs now run perpetually on the pi

--- egg/backend/scheduler.py
+++ egg/backend/scheduler.py
@@ -1,6 +1,35 @@
 from crontab import CronTab
 
-cron = CronTab(user='root')
-job = cron.new(command='echo "Running scheduler.py at $(date)" >> /home/djd5603/senior-design/egg/backend/logs/scheduler.log 2>1')
-job.minute.every(1)
-cron.write()
+def init(cron):
+    job = cron.new(command='egg/backend/update.sh >> /home/djd5603/senior-design/egg/backend/logs/scheduler.log 2>1')
+    job.minute.every(5)
+    cron.write()
+    return
+
+def create_job(cron):
+    job = cron.new(command='echo "Running scheduler.py at $(date)" >> /home/djd5603/senior-design/egg/backend/logs/scheduler.log 2>1')
+    job.minute.every(1)
+    cron.write()
+    return
+
+def delete_job(cron):
+    jobs = list(cron)
+    if jobs:
+        cron.remove(jobs[0])
+        cron.write()
+    return
+
+def main():
+    cron = CronTab(user='root')
+    
+    # init(cron)
+    # create_job(cron)
+    delete_job(cron)
+
+    for job in cron:
+        print(job)
+
+    return
+
+if __name__ == "__main__":
+    main()
