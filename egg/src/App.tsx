4b6c215ab172296036f79d80352efa0c33597428
Kaitlin Martin <kxm1944@mavs.uta.edu>
2025-07-15 21:21:11 -0500

    added home page

--- egg/src/App.tsx
+++ egg/src/App.tsx
@@ -41,7 +41,7 @@ function App() {
         
         {/* Add other pages here when you build them */}
         {activePage == 'home' && <Home />}
-        {/* add other pages here when you build them */}
+        {/* Add other pages here when you build them */}
       </div>
     </div>
   );
