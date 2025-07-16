eea69f1e627597c82ee04c972391122bbb452549
Kaitlin Martin <kxm1944@mavs.uta.edu>
2025-07-15 21:21:11 -0500

    added home page

--- egg/src/App.tsx
+++ egg/src/App.tsx
@@ -1,15 +1,17 @@
 import { useState } from 'react';
 import CareSchedule from './care-schedule/care-schedule';
+import Home from './home-page/home';
 import './App.css';
 
 const pages = [
   { id: 'care-schedule', label: 'Care Schedule' },
   { id: 'growth-compass', label: 'Growth Compass' },
   { id: 'machines', label: 'Machines' },
+  { id: 'home', label: 'Home'},
 ];
 
 function App() {
-  const [activePage, setActivePage] = useState<string | null>(null);
+  const [activePage, setActivePage] = useState<string>('home');
 
 
 
@@ -33,6 +35,7 @@ function App() {
       {/* Page container grows to fill the rest */}
       <div className="page-container">
         {activePage === 'care-schedule' && <CareSchedule />}
+        {activePage == 'home' && <Home />}
         {/* add other pages here when you build them */}
       </div>
     </div>
