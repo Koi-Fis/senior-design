7d87e8660704939da5a50fe3ebcf4c8251dccdc6
chanuthi <vidara54@gmail.com>
2025-07-17 18:13:19 -0500

    Recovered changes: sensor scripts, backend updates, care schedule work

--- egg/egg.sh
+++ egg/egg.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 /bin/sh -ec 'npm run dev &'
+<<<<<<< HEAD
 /bin/sh -ec 'python backend/app.py'
+=======
+/bin/sh -ec 'py -3 backend/app.py'
+>>>>>>> ea6444a (Recovered changes: sensor scripts, backend updates, care schedule work)
