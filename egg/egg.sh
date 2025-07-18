e02c9599a2219e741d797b5b8af716964aaec75e
Kaitlin Martin <kxm1944@mavs.uta.edu>
2025-07-17 20:18:58 -0500

    changed compile instructions

--- egg/egg.sh
+++ egg/egg.sh
@@ -1,7 +1,4 @@
 #!/bin/sh
 /bin/sh -ec 'npm run dev &'
-<<<<<<< HEAD
+# /bin/sh -ec 'py -3 backend/app.py'
 /bin/sh -ec 'python backend/app.py'
-=======
-/bin/sh -ec 'py -3 backend/app.py'
->>>>>>> ea6444a (Recovered changes: sensor scripts, backend updates, care schedule work)
