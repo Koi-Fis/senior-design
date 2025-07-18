f07c710eb52ea3c79b449caca338216f2a61f4c8
Kaitlin Martin <kxm1944@mavs.uta.edu>
2025-07-17 22:26:10 -0500

    oops

--- egg/backend/scheduler.py
+++ egg/backend/scheduler.py
@@ -1,41 +1,36 @@
 from crontab import CronTab
 
-cron = CronTab(user='root')
-job = cron.new(command='echo "Running scheduler.py at $(date)" >> /home/djd5603/senior-design/egg/backend/logs/scheduler.log 2>1')
-job.minute.every(1)
-cron.write()
+def init(cron):
+    job = cron.new(command='egg/backend/update.sh >> /home/djd5603/senior-design/egg/backend/logs/scheduler.log 2>1')
+    job.minute.every(5)
+    cron.write()
+    return
 
-# def init(cron):
-#     job = cron.new(command='egg/backend/update.sh >> /home/djd5603/senior-design/egg/backend/logs/scheduler.log 2>1')
-#     job.minute.every(5)
-#     cron.write()
-#     return
+def create_job(cron):
+    job = cron.new(command='echo "Running scheduler.py at $(date)" >> /home/djd5603/senior-design/egg/backend/logs/scheduler.log 2>1')
+    job.minute.every(1)
+    cron.write()
+    return
 
-# def create_job(cron):
-#     job = cron.new(command='echo "Running scheduler.py at $(date)" >> /home/djd5603/senior-design/egg/backend/logs/scheduler.log 2>1')
-#     job.minute.every(1)
-#     cron.write()
-#     return
+def delete_job(cron):
+    jobs = list(cron)
+    if jobs:
+        cron.remove(jobs[0])
+        cron.write()
+    return
 
-# def delete_job(cron):
-#     jobs = list(cron)
-#     if jobs:
-#         cron.remove(jobs[0])
-#         cron.write()
-#     return
-
-# def main():
-#     cron = CronTab(user='root')
+def main():
+    cron = CronTab(user='root')
     
-#     # init(cron)
-#     # create_job(cron)
-#     delete_job(cron)
+    # init(cron)
+    # create_job(cron)
+    delete_job(cron)
 
-#     print(len(cron), "jobs scheduled.")
-#     for job in cron:
-#         print(job)
+    print(len(cron), "jobs scheduled.")
+    for job in cron:
+        print(job)
 
-#     return
+    return
 
-# if __name__ == "__main__":
-#     main()
+if __name__ == "__main__":
+    main()
