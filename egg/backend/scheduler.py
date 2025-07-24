cd1a1db82b7387052c0ec71dc7bdb753c782c010
Koi-Fis <khoifish23@gmail.com>
2025-07-24 17:07:22 -0500

    adding water job from flask

--- egg/backend/scheduler.py
+++ egg/backend/scheduler.py
@@ -1,4 +1,5 @@
 from crontab import CronTab
+import sys
 
 def init(cron):
     job = cron.new(command='egg/backend/update.sh >> /home/djd5603/senior-design/egg/backend/logs/scheduler.log 2>1')
@@ -6,9 +7,9 @@ def init(cron):
     cron.write()
     return
 
-def create_job(cron):
+def create_job(cron, freq):
     job = cron.new(command='echo "Running scheduler.py at $(date)" >> /home/djd5603/senior-design/egg/backend/logs/scheduler.log 2>1')
-    job.minute.every(1)
+    job.minute.every(freq)
     cron.write()
     return
 
@@ -23,8 +24,8 @@ def main():
     cron = CronTab(user='root')
     
     # init(cron)
-    # create_job(cron)
-    delete_job(cron)
+    # create_job(cron, sys.argv[1])
+    # delete_job(cron)
 
     print(len(cron), "jobs scheduled.")
     for job in cron:
