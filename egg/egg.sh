3f4329c81937701d7bd364ff01716f28fd0b8aaf
Koi-Fis <khoifish23@gmail.com>
2025-07-24 16:30:45 -0500

    updating egg.sh

--- egg/egg.sh
+++ egg/egg.sh
@@ -17,8 +17,4 @@
 # npm run dev
 
 /bin/sh -ec 'npm run dev &'
-/bin/sh -ec 'py -3 backend/app.py'
-
-## /bin/sh -ec 'py -3 backend/app.py'
-#/bin/sh -ec 'python backend/app.py'
-
+/bin/sh -ec 'python backend/app.py'
