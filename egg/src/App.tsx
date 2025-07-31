ddd08cdd49b564330fa79520159025579cbf898c
Kaitlin Martin <kxm1944@mavs.uta.edu>
2025-07-30 22:18:37 -0500

    removed refresh smh

--- egg/src/App.tsx
+++ egg/src/App.tsx
@@ -12,7 +12,7 @@ const pages = [
 ];
 
 function App() {
-  const [activePage, setActivePage] = useState<string>('care-schedule');
+  const [activePage, setActivePage] = useState<string>('home');
 
 
 
