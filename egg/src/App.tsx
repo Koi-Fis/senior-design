53011a25c25ef2e43cff041db25eadc9bf3ff4f7
Kaitlin Martin <kxm1944@mavs.uta.edu>
2025-07-31 20:16:49 -0500

    added global plant-selection from home

--- egg/src/App.tsx
+++ egg/src/App.tsx
@@ -3,6 +3,7 @@ import CareSchedule from './care-schedule/care-schedule';
 import GrowthCompass from './growth-compass/growth-compass';
 import Home from './home-page/home';
 import './App.css';
+import { PlantProvider } from './plant-context/plant-selection';
 
 const pages = [
   { id: 'home', label: 'Home'},
@@ -16,35 +17,39 @@ function App() {
 
 
   return (
-    <div className="app-shell">
-      {/* Sidebar gets a fixed width */}
-      <div className="sidebar">
-        <div className="logo-container">
-          <div className="logo">
+    <PlantProvider>
+      {/* <Routes> */}
+        <div className="app-shell">
+          {/* Sidebar gets a fixed width */}
+          <div className="sidebar">
+            <div className="logo-container">
+              <div className="logo">
+
+              </div>
+            </div>
+            {pages.map(page => (
+              <button
+                key={page.id}
+                className={`btn   ${activePage === page.id ? 'active' : ''}`}
+                onClick={() => setActivePage(page.id)}
+              >
+                <span>{page.label}</span>
+              </button>
+            ))}
+            
 
           </div>
+            
+          {/* Page container grows to fill the rest */}
+          <div className="page-container">
+            {activePage == 'home' && <Home />}
+            {activePage === 'care-schedule' && <CareSchedule />}
+            {activePage === 'growth-compass' && <GrowthCompass />}
+            {/* add other pages here when you build them */}
+          </div>
         </div>
-        {pages.map(page => (
-          <button
-            key={page.id}
-            className={`btn   ${activePage === page.id ? 'active' : ''}`}
-            onClick={() => setActivePage(page.id)}
-          >
-            <span>{page.label}</span>
-          </button>
-        ))}
-        
-
-      </div>
-
-      {/* Page container grows to fill the rest */}
-      <div className="page-container">
-        {activePage == 'home' && <Home />}
-        {activePage === 'care-schedule' && <CareSchedule />}
-        {activePage === 'growth-compass' && <GrowthCompass />}
-        {/* add other pages here when you build them */}
-      </div>
-    </div>
+      {/* </Routes> */}
+    </PlantProvider>
   );
 }
 
